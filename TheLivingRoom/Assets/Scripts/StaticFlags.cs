649860e33fea8f6f09d9e66f87b203293bf04cf5
Antoton <antoton@gmail.com>
2020-12-07 13:29:25 +0100

    Added StaticFlags

---
+++ TheLivingRoom/Assets/Scripts/StaticFlags.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StaticFlags
+{
+    public static bool InteractedWithCellphone1 = false;
+    public static bool InteractedWithFaucet1 = false;
+}
