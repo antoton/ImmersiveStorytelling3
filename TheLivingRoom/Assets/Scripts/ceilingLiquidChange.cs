0c37e7189d62b74a7743833011559a29bfc8a5a7
QuintenS <quinten.schutyser@gmail.com>
2020-12-18 15:09:55 +0100

    added some comments in script

--- TheLivingRoom/Assets/Scripts/ceilingLiquidChange.cs
+++ TheLivingRoom/Assets/Scripts/ceilingLiquidChange.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class ceilingLiquidChange : MonoBehaviour
 {
+    [Tooltip("Witch images need to be shown")]
     public List<Texture> image; //list with all the pictures
+    [Tooltip("after how mutch time need the ceiling change")]
     public int time = 10;
 
     private int currentImage = 0; //current image shown
@@ -12,26 +14,23 @@ public class ceilingLiquidChange : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("changeCeiling", time, time);
+        InvokeRepeating("changeCeiling", time, time); //repeate function with timer
     }
 
     // Update is called once per frame
     void Update()
     {
-        //ceiling.GetComponent
+
     }
 
     void changeCeiling()
     {
-        print("current image: " + currentImage);
-        print("image count: " + image.Count);
         if (image.Count > 0)
         {
             if (currentImage <= (image.Count-1))
             {
                 GetComponent<Renderer>().material.SetTexture("_MainTex", image[currentImage]);
                 currentImage++;
-                print("in function");
 
             }
         }
